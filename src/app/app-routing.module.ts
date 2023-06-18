b4cb057dcfb2742146a6c23eb3f9620b2c20f2a7
Sahil-107 <messisahil7@gmail.com>
2023-06-19 02:07:44 +0530

    dont know

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotesComponent } from './notes/notes.component';
+import { DesktopComponent } from './desktop/desktop.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent
+  },
+  {
+    path: '',
+    component: DesktopComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
